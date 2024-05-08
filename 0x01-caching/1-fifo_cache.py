13a64b34e0a396bf12739bb6ec4513dc5125406c
MentalSep <chahidjava@hotmail.com>
2024-05-09 00:41:56 +0100

    add task 2 & improve task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,16 +7,22 @@ class FIFOCache(BaseCaching):
     """ Defines a caching system using FIFO eviction policy """
     def __init__(self):
         super().__init__()
+        self.cache_order = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                first_key = next(iter(self.cache_data))
-                if key not in self.cache_data:
-                    del self.cache_data[first_key]
-                    print("DISCARD:", first_key)
+                if key in self.cache_data:
+                    self.cache_order.pop(0)
+                else:
+                    discarded_key = self.cache_order[0]
+                    del self.cache_data[discarded_key]
+                    print("DISCARD:", discarded_key)
+
             self.cache_data[key] = item
+            if key not in self.cache_order:
+                self.cache_order.append(key)
 
     def get(self, key):
         """ Get an item by key """
