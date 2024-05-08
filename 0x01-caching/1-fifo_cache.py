4871d969e3d48c9571cbd2fd886eafc3715e07b2
MentalSep <chahidjava@hotmail.com>
2024-05-08 23:34:49 +0100

    fix task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,8 +13,9 @@ class FIFOCache(BaseCaching):
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 first_key = next(iter(self.cache_data))
-                del self.cache_data[first_key]
-                print("DISCARD:", first_key)
+                if key not in self.cache_data:
+                    del self.cache_data[first_key]
+                    print("DISCARD:", first_key)
             self.cache_data[key] = item
 
     def get(self, key):
