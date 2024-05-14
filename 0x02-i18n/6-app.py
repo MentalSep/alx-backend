08e5dc5b22ec5be77599a6a2e491ce3cdcfc0e81
MentalSep <chahidjava@hotmail.com>
2024-05-14 22:59:42 +0100

    add task 6

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id):
+    """Retrieves user information from the mock user table"""
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """Sets the user as a global variable based on login_as parameter."""
+    user_id = request.args.get('login_as', type=int)
+    g.user = get_user(user_id) if user_id else None
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    user = getattr(g, 'user', None)
+    if user and user.get('locale') in app.config['LANGUAGES']:
+        return user.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def render():
+    """ Render a template """
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
